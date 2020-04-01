9cfddcf94774894dff6e1978ac375215c0815a4c
Hassan Raza Bukhari <51364060+eruditehassan@users.noreply.github.com>
2020-04-01 18:50:47 +0500

    Update New York Restaurants Project.sql

--- Codecademy Coursework/SQL_Basics/New York Restaurants Project.sql
+++ Codecademy Coursework/SQL_Basics/New York Restaurants Project.sql
@@ -1,55 +1,55 @@
-#1 Start by getting a feel for the nomnom table
+-- 1 Start by getting a feel for the nomnom table
 SELECT *
 FROM nomnom;
 
-#2 What are the distinct neighborhoods?
+-- 2 What are the distinct neighborhoods?
 SELECT DISTINCT neighborhood
 FROM nomnom;
 
-#3 What are the distinct cuisine types?
+-- 3 What are the distinct cuisine types?
 SELECT DISTINCT cuisine
 FROM nomnom;
 
-#4 Suppose we would like some Chinese takeout. What are our options?
+-- 4 Suppose we would like some Chinese takeout. What are our options?
 SELECT *
 FROM nomnom
 WHERE cuisine = 'Chinese';
 
-#5 Return all the restaurants with reviews of 4 and above
+-- 5 Return all the restaurants with reviews of 4 and above
 SELECT *
 FROM nomnom
 WHERE review >= 4;
 
-#6 Suppose Abbi and Ilana want to have a fancy dinner date. Return all the restaurants that are Italian and $$$.
+-- 6 Suppose Abbi and Ilana want to have a fancy dinner date. Return all the restaurants that are Italian and $$$.
 SELECT *
 FROM nomnom
 WHERE cuisine = 'Italian'
    AND price = '$$$';
 
-#7 Your coworker Trey can’t remember the exact name of a restaurant he went to but he knows it contains the word ‘meatball’ in it. Can you find it for him using a query?
+-- 7 Your coworker Trey can’t remember the exact name of a restaurant he went to but he knows it contains the word ‘meatball’ in it. Can you find it for him using a query?
 SELECT *
 FROM nomnom
 WHERE name LIKE '%meatball%';
 
-#8 Let’s order delivery to the house! Find all the close by spots in Midtown, Downtown or Chinatown
+-- 8 Let’s order delivery to the house! Find all the close by spots in Midtown, Downtown or Chinatown
 SELECT *
 FROM nomnom
 WHERE neighborhood = 'Midtown'
    OR neighborhood = 'Downtown'
    OR neighborhood = 'Chinatown'; 
 
-#9 Find all the health grade pending restaurants (empty values).
+-- 9 Find all the health grade pending restaurants (empty values).
 SELECT *
 FROM nomnom
 WHERE health IS NULL;
 
-#10 Create a Top 10 Restaurants Ranking based on reviews.
+-- 10 Create a Top 10 Restaurants Ranking based on reviews.
 SELECT *
 FROM nomnom
 ORDER BY review DESC
 LIMIT 10;
 
-#11 Use a CASE statement to change the rating system
+-- 11 Use a CASE statement to change the rating system
 select Case
   when review>4.5 then 'Extraordinary'
   when review > 4 then 'Excellent'
